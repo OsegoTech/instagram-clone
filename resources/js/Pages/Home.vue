56f893f23a799018a55b4dcc5ea0da96bab0fbc5
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-08-01 23:28:36 +0300

    initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <MainLayout>
+        <div class="bg-green-500">HOME.VUE</div>
+    </MainLayout>
+</template>
